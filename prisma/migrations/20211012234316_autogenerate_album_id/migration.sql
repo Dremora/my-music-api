c2946f46df59d19e0f59e17d2d01869ec45280df
Kirill Korolyov <kirill.korolyov@gmail.com>
2021-10-13 00:56:54 +0100

    Use autogenerated album id

---
+++ prisma/migrations/20211012234316_autogenerate_album_id/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "album_sources" DROP CONSTRAINT "album_sources_album_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "album_sources" ADD CONSTRAINT "album_sources_album_id_fkey" FOREIGN KEY ("album_id") REFERENCES "albums"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
