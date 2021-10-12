8c0d687526dbcdec9c431eb793d44792dbf2e93c
Kirill Korolyov <kirill.korolyov@gmail.com>
2021-10-12 19:06:22 +0100

    Await for the server to start inside lambda handler

--- api/index.ts
+++ api/index.ts
@@ -5,4 +5,17 @@ import { schema } from "../src/schema";
 
 const server = new ApolloServer({ schema, context });
 
-export default server.createHandler({ path: "/api" });
+const startServer = server.start();
+
+export default async function handler(req, res) {
+  await startServer;
+  await server.createHandler({
+    path: "/api",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
