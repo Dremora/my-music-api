c95a83b522268678de048767a8620c0541e86bef
Kirill Korolyov <kirill.korolyov@gmail.com>
2021-10-12 19:17:36 +0100

    Fix type error on the handler

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,4 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { ApolloServer } from "apollo-server-micro";
 
 import { context } from "../src/context";
@@ -7,12 +8,15 @@ const server = new ApolloServer({ schema, context, introspection: true });
 
 const startServer = server.start();
 
-export default async function handler(req, res) {
+export default async (
+  req: VercelRequest,
+  res: VercelResponse
+): Promise<void> => {
   await startServer;
   await server.createHandler({
     path: "/api",
   })(req, res);
-}
+};
 
 export const config = {
   api: {
