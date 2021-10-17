f20bb00a2555cd2d9705d3ee0b4a8412b4106480
Kirill Korolyov <kirill.korolyov@gmail.com>
2021-10-17 17:25:59 +0100

    Make optional fields nullable as well

--- src/domain/album.ts
+++ src/domain/album.ts
@@ -8,23 +8,23 @@ import { PrismaClient } from ".prisma/client";
 const trim = (str: string) => str.trim();
 
 const newSourceSchema = z.object({
-  accurateRip: z.optional(z.string().max(255).transform(trim)),
-  comments: z.optional(z.string().max(255).transform(trim)),
-  cueIssues: z.optional(z.string().max(255).transform(trim)),
-  discs: z.optional(z.number().int().min(1).max(100)),
-  download: z.optional(z.string().max(255).transform(trim)),
-  edition: z.optional(z.string().max(255).transform(trim)),
-  format: z.optional(z.enum(Format.members)),
+  accurateRip: z.nullable(z.optional(z.string().max(255).transform(trim))),
+  comments: z.nullable(z.optional(z.string().max(255).transform(trim))),
+  cueIssues: z.nullable(z.optional(z.string().max(255).transform(trim))),
+  discs: z.nullable(z.optional(z.number().int().min(1).max(100))),
+  download: z.nullable(z.optional(z.string().max(255).transform(trim))),
+  edition: z.nullable(z.optional(z.string().max(255).transform(trim))),
+  format: z.nullable(z.optional(z.enum(Format.members))),
   location: z.enum(Location.members),
-  mbid: z.optional(z.string().length(36)), // TODO regex?
-  tagIssues: z.optional(z.string().max(255).transform(trim)),
+  mbid: z.nullable(z.optional(z.string().length(36))), // TODO regex)?
+  tagIssues: z.nullable(z.optional(z.string().max(255).transform(trim))),
 });
 
 const newAlbumSchema = z.object({
   artist: z.string().min(1).max(255).transform(trim),
   title: z.string().min(1).max(255).transform(trim),
-  comments: z.optional(z.string().max(255).transform(trim)),
-  year: z.optional(z.number().int().min(1900).max(2100)),
+  comments: z.nullable(z.optional(z.string().max(255).transform(trim))),
+  year: z.nullable(z.optional(z.number().int().min(1900).max(2100))),
   sources: z.array(newSourceSchema),
 });
 
